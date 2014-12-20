496e3ac0ce337e7fb9e28ee88549423b63bf4e12
John Steel <jsteel-cc@conestogac.on.ca>
2014-12-20 16:27:14 -0500

    Unit tests now pass.

--- LinkList/LinkList.cs
+++ LinkList/LinkList.cs
@@ -2,7 +2,7 @@
 
 namespace LinkList
 {
-    class LinkList<T>
+    public class LinkList<T>
     {
         private LinkListItem<T> _first;
         private LinkListItem<T> _last;
@@ -26,7 +26,7 @@ namespace LinkList
             var listItem = _first;
             if (listItem == null)
             {
-                throw new Exception("Nothing to pop");
+                return default(T);
             }
             _first = _first.Next;
             if (_first != null)
@@ -37,15 +37,70 @@ namespace LinkList
             return listItem.Element;
         }
 
-        static void Main(string[] args)
+        private LinkListItem<T> SelectAt(int index)
         {
-            var linkList = new LinkList<int>();
-            linkList.Add(30);
-            Console.WriteLine(linkList.Pop() == 30);
+            if(index < 0 || index >= _length){
+                return null;
+            }
+            int steps;
+            LinkListItem<T> pointer;
+
+            if(index < _length / 2){
+                steps = index;
+                pointer = _first;
+                while(steps > 0 && pointer != null){
+                    pointer = pointer.Next;
+                    steps--;
+                }
+            } else {
+                steps = _length - index - 1;
+                pointer = _last;
+                while(steps > 0 && pointer != null){
+                    pointer = pointer.Prev;
+                    steps--;
+                }
+            }
+            return pointer;
+        }
+
+        public T GetElementByIndex(int index)
+        {
+            var selection = SelectAt(index);
+            return selection == null ? default(T) : selection.Element;
+        }
+
+        public LinkListItem<T> GetObjectByIndex(int index)
+        {
+            return SelectAt(index);
+        }
+
+        public int Recount()
+        {
+            int length = 0;
+            LinkListItem<T> pointer = _first;
+            while(pointer != null){
+                length++;
+                pointer = pointer.Next;
+            }
+            return length;
+        }
+
+        public int Length
+        {
+            get { return _length; }
+        }
+
+        public LinkListItem<T> FirstObject
+        {
+            get { return _first; }
+        }
+
+        public LinkListItem<T> LastObject
+        {
+            get { return _last; }
         }
     }
-
-    class LinkListItem<T>
+    public class LinkListItem<T>
     {
         public LinkListItem<T> Prev;
         public LinkListItem<T> Next;
---
+++ LinkListTests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using LinkList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinkListTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var linkList = new LinkList<int?>();
+            linkList.Add(30);
+
+            Assert.IsTrue(linkList.Pop() == 30, "Pop the 30");
+            Assert.IsNull(linkList.Pop(), "Pop null");
+            
+            linkList.Add(20);
+            linkList.Add(10);
+            Assert.IsTrue(linkList.Pop() == 20, "Pop the 20");
+            Assert.IsNull(linkList.FirstObject.Prev, "First Prev Null");
+            Assert.IsTrue(linkList.Pop() == 10, "Pop the 10");
+
+            for (var i = 0; i < 10; i++)
+            {
+                linkList.Add(i);
+            }
+
+            Assert.IsTrue(linkList.GetElementByIndex(4) == 4, "index 4 is 4");
+            Assert.IsTrue(linkList.GetElementByIndex(6) == 6, "index 6 is 6");
+
+            Assert.IsTrue(linkList.GetObjectByIndex(6) == linkList.GetObjectByIndex(4).Next.Next, "object 6 is 4.next.next");
+            Assert.IsTrue(linkList.GetObjectByIndex(4) == linkList.GetObjectByIndex(6).Prev.Prev, "object 4 is 6.next.next");
+
+            Assert.IsTrue(linkList.GetObjectByIndex(0) == linkList.FirstObject, "last is 9");
+            Assert.IsTrue(linkList.GetObjectByIndex(9) == linkList.LastObject, "first is 0");
+        }
+    }
+}
