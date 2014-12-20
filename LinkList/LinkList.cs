29ead988c964f7a6b1b39f255bcc5258d9520bb5
John Steel <jsteel-cc@conestogac.on.ca>
2014-12-20 14:35:31 -0500

    Init

---
+++ LinkList/LinkList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace LinkList
+{
+    class LinkList<T>
+    {
+        private LinkListItem<T> _first;
+        private LinkListItem<T> _last;
+        private int _length;
+
+        public int Add(T element)
+        {
+            if (_length == 0)
+            {
+                _last = _first = new LinkListItem<T> { Element = element, Prev = null, Next = null };
+            }
+            else
+            {
+                _last = _last.Next = new LinkListItem<T> { Element = element, Prev = _last, Next = null };
+            }
+            return ++_length;
+        }
+
+        public T Pop()
+        {
+            var listItem = _first;
+            if (listItem == null)
+            {
+                throw new Exception("Nothing to pop");
+            }
+            _first = _first.Next;
+            if (_first != null)
+            {
+                _first.Prev = null;
+            }
+            _length--;
+            return listItem.Element;
+        }
+
+        static void Main(string[] args)
+        {
+            var linkList = new LinkList<int>();
+            linkList.Add(30);
+            Console.WriteLine(linkList.Pop() == 30);
+        }
+    }
+
+    class LinkListItem<T>
+    {
+        public LinkListItem<T> Prev;
+        public LinkListItem<T> Next;
+        public T Element;
+    }
+}
